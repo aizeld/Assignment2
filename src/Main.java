e08c1f4bd5460e984b524933d22475f2cc3987a7
aizeld <ismailov1314@gmail.com>
2023-04-14 21:47:28 +0600

    Tested all methods with commentations

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,15 @@ public class Main {
         System.out.println("Hello world!");
         MyArraylist<Integer> mylist = new MyArraylist<>();
 
-        mylist.add(4);
+        mylist.add(4); ///just adding some elements
         mylist.add(5);
-        mylist.add(3);
+        mylist.add(7);
 
-        System.out.println(mylist.size());
+
+        System.out.println("size of list " + mylist.size()); ///3
+        System.out.println("element at index 2 is " + mylist.get(2));///7
+        mylist.remove(2);
+        System.out.println("checking the size after removing " + mylist.size());///2
 
     }
 }
