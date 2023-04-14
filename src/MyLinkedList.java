2635460c8f5f74f927d790770104b6af88a37ac2
aizeld <ismailov1314@gmail.com>
2023-04-14 21:48:57 +0600

    created Mylinkedlist

---
+++ src/MyLinkedList.java
@@ -0,0 +1,21 @@
+public class MyLinkedList <T> implements MyList<T>{
+    @Override
+    public void add(T element) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
