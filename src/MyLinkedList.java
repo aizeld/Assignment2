49b99236f1bfcd503f6bd3b366091c53a78d716b
aizeld <ismailov1314@gmail.com>
2023-04-14 21:54:52 +0600

    Defined a private Node Defined a head Defined a tael Defined a size

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,4 +1,21 @@
 public class MyLinkedList <T> implements MyList<T>{
+
+    private class Node(){
+        T element;
+        Node next;
+        Node previous;
+        public Node(T element, Node previous, Node next){
+            this.element = element;
+            this.next = next;
+            this.previous = previous;
+        }
+    }
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyLinkedList
+
     @Override
     public void add(T element) {
 
