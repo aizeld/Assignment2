7d952b8f9bc7f4092c6734fdbb616fc55f9042ae
aizeld <ismailov1314@gmail.com>
2023-04-14 21:27:33 +0600

    Defined a private instance variable of type Object[] to hold the elements of the list and Defined an int variable called size to keep track of the number of elements in the list.

--- src/MyArraylist.java
+++ src/MyArraylist.java
@@ -1,6 +1,29 @@
-import java.util.Iterator;
 public class MyArraylist<T> implements MyList<T> {
 private Object[] hiddenArr;
 private int length;
 
+public MyArraylist(){
+    hiddenArr = new Object[10];
+    length = 0;
+}
+
+    @Override
+    public void add(T element) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
--- src/MyList.java
+++ src/MyList.java
@@ -1,3 +1,6 @@
-public interface MyList<T> extends Iterable<T>{
-    ;
+public interface MyList<T>{
+    void add(T element);
+    T get(int index);
+    void remove(int index);
+    int size();
 }
